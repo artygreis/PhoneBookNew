6c15b6c801d850dbe42107d5cd7fa137df0257b8
Артём Грейс <artem_greis@mail.ru>
2023-08-22 15:52:22 +0300

    Корректировка обновления

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,3 +1,5 @@
+using Squirrel;
+
 namespace PhoneBook
 {
     internal static class Program
@@ -11,9 +13,19 @@ namespace PhoneBook
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTc4OTgwN0AzMjMxMmUzMTJlMzMzNUFrQnBYTVlYeDhMbkRNN3pkOVA3NGZXZ0l2TFRPY0g2MUxMcnpzUEIwemc9");
-
+            
+            SquirrelAwareApp.HandleEvents(onInitialInstall: OnAppInstall, onAppUninstall: OnAppUninstall);
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
+        }
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
         }
     }
 }
